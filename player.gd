d0d9f4b09601b23a1ac59633744f2ecf1b55a077
Lexani Turu-Bates <lextur16@kingshigh.school.nz>
2026-04-28 12:22:44 +1200

    save 2
    added end scene function
    end scene function failed
    removed ball
    fixed grounds mesh clashing

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var body = $CollisionShape3D
 @export var mouse_sens = 0.002
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -36,3 +37,6 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _on_exit_area_entered(area: Area3D) -> void:
+	get_tree().quit()
