dfb59a000794e4934d96120b56000708aa6ff152
Lexani Turu-Bates <lextur16@kingshigh.school.nz>
2026-05-12 09:54:54 +1200

    save 6

--- enemy.gd
+++ enemy.gd
@@ -13,3 +13,7 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = new_velocity
 	move_and_slide()
+	
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		get_tree().call_deferred("reload_current_scene")
