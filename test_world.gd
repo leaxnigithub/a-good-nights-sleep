0ee1dc48b905d8bbc11c7855674927a3ded9ea9b
Lexani Turu-Bates <lextur16@kingshigh.school.nz>
2026-06-04 14:59:09 +1200

    save 12

--- test_world.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-@onready var player = $player
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	get_tree().call_group("enemy", "update_location", player.global_transform.origin)
